e44d54198401534a8d25d9901eef73a38079224d
Mehmet Arslan <marslan.developer@gmail.com>
2022-09-01 15:12:36 +0300

    finished

---
+++ app.js
@@ -0,0 +1,6 @@
+const reverseSentence = (sentence) => {
+  console.log(sentence.split(" ").reverse().join(" "));
+};
+
+reverseSentence(`days. big make things Little`);
+// output : Little things make big days.
